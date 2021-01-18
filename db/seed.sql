0f45cebcd6fd42c0efe56aed064568ded7852cd9
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2021-01-18 17:05:42 -0500

    minor updates

--- db/seed.sql
+++ db/seed.sql
@@ -15,17 +15,17 @@ INSERT INTO role
 VALUES
 	("manager", 80000.00, 1),
 	("developer", 65000.00, 2),
-    ("account rep", 40000.00, 3),
+    ("account rep", 40000.00, 2),
     ("assitant", 24000.00, 4),
-    ("sales", 35000.00, 5);
+    ("sales", 35000.00, 4);
 
 ---- employees
 INSERT INTO employee 
 	(first_name, last_name, role_id, manager_id)
 VALUES
-	("Bob", "Corben", 1, 1),
-    ("Libby", "Smith", 2, 2),
-    ("Tammy", "Decker", 3, 3),
-    ("Jeremy", "Tanner", 4, 4),
-    ("James", "Tanner", 5, 5);
+	("Bob", "Corben", 1, null),
+    ("Libby", "Smith", 2, 1),
+    ("Tammy", "Decker", 3, 1),
+    ("Jeremy", "Tanner", 4, 1),
+    ("James", "Tanner", 5, 1);
     
